af753d45c93c18a3527359292a4893a271351a7e
Tolulope Odueke <tolulope.od@gmail.com>
2019-02-13 12:35:33 +0100

    (chore) Refactor test route

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import mealsRoute from './server/routes/mealsRoute';
 // Set up the app with express
 const app = express();
 
+/* Use to test server
 app.get('/test', (req, res) => {
   res.status(200).send('Response Returned Successfully');
 });
-
+*/
 app.use('/api/v1/meals', mealsRoute);
 
 const PORT = 8080;
