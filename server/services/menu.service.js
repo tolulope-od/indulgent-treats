035064b8e7d599de0c61df568b5c6a7f17f52017
Tolulope Odueke <tolulope.od@gmail.com>
2019-02-16 01:48:11 +0100

    Fix menu and order dummy data

--- server/services/menu.service.js
+++ server/services/menu.service.js
@@ -1,28 +1,29 @@
 import MealsService from './meal.service';
+import dummyData from '../utils/dummyData';
 
-const Menu = [];
+const { menu } = dummyData;
 
 const MenuService = {
   fetchMenu() {
-    return Menu;
+    return menu;
   },
   setMenu() {
     const newMenu = MealsService.fetchAllMeals();
-    newMenu.map(items => Menu.push(items));
-    return Menu;
+    newMenu.map(items => menu.push(items));
+    return menu;
   },
   addAMeal(id) {
     const newMeal = MealsService.getAMeal(id);
     // Check to make sure that the id entered is a valid id of an existing meal
-    if (Object.entries(newMeal).length === 0 || !Menu.indexOf(newMeal)) {
+    if (Object.entries(newMeal).length === 0 || !menu.indexOf(newMeal)) {
       return {};
     }
     // Check if meal exists already
     // eslint-disable-next-line eqeqeq
-    const findMeal = Menu.some(meal => meal.id == newMeal.id);
+    const findMeal = menu.some(meal => meal.id == newMeal.id);
     if (!findMeal) {
-      Menu.push(newMeal);
-      return Menu;
+      menu.push(newMeal);
+      return menu;
     }
     return newMeal || {};
   },
